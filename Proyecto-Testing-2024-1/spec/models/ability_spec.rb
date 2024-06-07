ab9f508a61897486f4fceb1b94f10fed6828d8ff
Scagender <scagender@scagender>
2024-06-07 16:56:41 -0400

    test unitarios

--- Proyecto-Testing-2024-1/spec/models/ability_spec.rb
+++ Proyecto-Testing-2024-1/spec/models/ability_spec.rb
@@ -2,68 +2,77 @@ require 'rails_helper'
 require 'cancan/matchers'
 
 RSpec.describe Ability, type: :model do
-  describe "abilities" do
+  describe 'abilities' do
     subject(:ability) { Ability.new(user) }
     let(:user) { nil }
 
-    context "when user is an admin" do
+    context 'when user is an admin' do
       let(:user) { User.new(name: 'John1', password: 'Nonono123!', email: 'asdf@gmail.com', role: 'admin') }
 
       it { should be_able_to(:manage, :all) }
     end
 
-    context "when user is present" do
+    context 'when user is present' do
       let(:user) { User.new(name: 'John1', password: 'Nonono123!', email: 'asdf@gmail.com') }
-
-      it { should be_able_to(:index, Product) }
-      it { should be_able_to(:leer, Product) }
-      it { should be_able_to(:insertar, Product) }
-      it { should be_able_to(:crear, Product) }
-
-      it { should be_able_to(:index, Review) }
-      it { should be_able_to(:leer, Review) }
-      it { should be_able_to(:insertar, Review) }
-      it { should be_able_to(:crear, Review) }
-
-      it { should be_able_to(:leer, Message) }
-      it { should be_able_to(:insertar, Message) }
-
-      it { should be_able_to(:index, Solicitud) }
-
       it { should_not be_able_to(:insert_deseado, user.products.build) }
       it { should_not be_able_to(:insertar, user.solicituds.build) }
 
-      context "when product belongs to user" do
-        let(:product) { Product.new(user: user) }
-
+      context 'when product belongs to user' do
+        let(:product) { Product.create(
+          nombre: 'Producto de prueba 2',
+          precio: 4000,
+          stock: 1,
+          user: user,
+          categories: 'Cancha'
+        ) }
         it { should be_able_to(:eliminar, product) }
         it { should be_able_to(:actualizar_producto, product) }
         it { should be_able_to(:actualizar, product) }
-        #it { should_not be_able_to(:eliminar, user.solicituds.build(product: product)) }
-        #it { should_not be_able_to(:actualizar, user.solicituds.build(product: product)) }
+        it { should be_able_to(:eliminar, user.solicituds.build(product: product)) } 
+        it { should be_able_to(:actualizar, user.solicituds.build(product: product)) }
       end
 
-      context "when solicitud belongs to user" do
-        let(:user) { User.new(name: 'John1', password: 'Nonono123!', email: 'asdf@gmail.com') }
-        let(:product) { Product.new(user: user) }
+      context 'when product does not belong to user' do
+        let(:other_product) { Product.create(
+          nombre: 'Otro producto',
+          precio: 2000,
+          stock: 1,
+          user: User.create(name: 'Otro usuario', password: 'password123', email: 'otro@example.com', role: 'normal'),
+          categories: 'Algo'
+        ) }
+        it { should_not be_able_to(:eliminar, other_product) }
+        it { should_not be_able_to(:actualizar, other_product) }
+      end
+
+      context 'when solicitud belongs to user' do
+        let(:product) { Product.create(
+          nombre: 'Producto de prueba 2',
+          precio: 4000,
+          stock: 1,
+          user: user,
+          categories: 'Cancha'
+        ) }
         let(:solicitud) { Solicitud.new(user: user, product: product) }
 
-        #it { should be_able_to(:eliminar, solicitud) }
         it { should be_able_to(:leer, solicitud) }
-        #it { should_not be_able_to(:actualizar, solicitud) }
+        it { should be_able_to(:eliminar, solicitud) }
       end
 
-      context "when solicitud's product belongs to user" do
-        let(:solicitud) { Solicitud.new }
-        let(:product) { Product.new(user: user) }
-        
+      context 'when solicituds product belongs to user' do
+        let(:solicitud) { Solicitud.new(product: product) }
+        let(:product) { Product.create(
+          nombre: 'Producto de prueba 2',
+          precio: 4000,
+          stock: 1,
+          user: user,
+          categories: 'Cancha'
+        ) }
         before { solicitud.product = product }
-
-        #it { should be_able_to(:eliminar, solicitud) }
-        #it { should be_able_to(:actualizar, solicitud) }
+        it { should be_able_to(:eliminar, solicitud) } 
+        it { should be_able_to(:actualizar, solicitud) }
       end
 
-      context "when review belongs to user" do
+      context 'when review belongs to user' do
         let(:review) { Review.new(user: user) }
 
         it { should be_able_to(:eliminar, review) }
@@ -71,22 +80,19 @@ RSpec.describe Ability, type: :model do
         it { should be_able_to(:actualizar, review) }
       end
 
-      context "when message belongs to user" do
+      context 'when message belongs to user' do
         let(:message) { Message.new(user: user) }
 
         it { should be_able_to(:eliminar, message) }
       end
     end
 
-    context "when user is nil" do
+    context 'when user is nil' do
       it { should be_able_to(:index, Product) }
       it { should be_able_to(:leer, Product) }
-
       it { should be_able_to(:index, Review) }
       it { should be_able_to(:leer, Review) }
-
       it { should be_able_to(:leer, Message) }
-
       it { should_not be_able_to(:manage, :all) }
       it { should_not be_able_to(:insertar, Product) }
       it { should_not be_able_to(:crear, Product) }
@@ -94,6 +100,8 @@ RSpec.describe Ability, type: :model do
       it { should_not be_able_to(:crear, Review) }
       it { should_not be_able_to(:insertar, Message) }
       it { should_not be_able_to(:index, Solicitud) }
+      it { should_not be_able_to(:actualizar_producto, Product) }
+
     end
   end
 end
